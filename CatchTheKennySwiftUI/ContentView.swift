a57c4b9932e1ca810f4ebaea85edf7c1177ea47b
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-08 19:28:16 +0300

    Initial Commit

---
+++ CatchTheKennySwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CatchTheKennySwiftUI
+//
+//  Created by Atil Samancioglu on 8.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
